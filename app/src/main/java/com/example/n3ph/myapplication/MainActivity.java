d51ed761172ff0cb232eb8b6400601e1b69b875f
TJ Buchannan <tj.buchannan@gmail.com>
2015-02-21 19:58:36 -0500

    Added sound to button press, cleaned up MainActivity.

--- app/src/main/java/com/example/n3ph/myapplication/MainActivity.java
+++ app/src/main/java/com/example/n3ph/myapplication/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.n3ph.myapplication;
 
 import android.graphics.Color;
+import android.media.AudioManager;
+import android.media.SoundPool;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -12,26 +14,38 @@ import android.widget.TextView;
 import java.util.Random;
 
 
+
 public class MainActivity extends ActionBarActivity {
+    SoundPool mySound;
+    TextView textView3;
+    TextView theScore;
+    Button button;
+    RelativeLayout root;
+    Random rand;
+    int vegetaHuahId;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final TextView textView3 =(TextView) findViewById(R.id.whatUp);
-        final TextView theScore = (TextView) findViewById(R.id.keepScore);
-        Button button = (Button) findViewById(R.id.clickMeButton);
-        final RelativeLayout root =(RelativeLayout)findViewById(R.id.root);
-        final Random rand = new Random();
+        mySound = new SoundPool(1, AudioManager.STREAM_MUSIC, 0);
+        vegetaHuahId = mySound.load(this, R.raw.vegeta_huah, 1);
+        textView3 =(TextView) findViewById(R.id.whatUp);
+        theScore = (TextView) findViewById(R.id.keepScore);
+        button = (Button) findViewById(R.id.clickMeButton);
+        root =(RelativeLayout)findViewById(R.id.root);
+        rand = new Random();
+
 
         button.setOnClickListener(new View.OnClickListener() {
-
             /* When the button is clicked change the background color to a random color and
             * change the text*/
             int flag = 1;
             @Override
              public void onClick(View v) {
+                 mySound.play(vegetaHuahId, 1, 1, 1, 0, 1f);
 
                 //Random color generator
                 int r = rand.nextInt(255);
@@ -72,7 +86,6 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
